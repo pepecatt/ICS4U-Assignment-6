d927473654ff4998c8f85b25f97c9a75a2d3c00e
Catherine <catlih28@gmail.com>
2024-09-25 19:25:09 -0400

    created a while loop that takes in user input until they enter something that isn't an integer, and prints the updated counter

--- Assignment1.java
+++ Assignment1.java
@@ -2,8 +2,18 @@ import java.util.*;
 public class Assignment1 {
     public static void main(String[] args) {
         int counter = 0;
-        String input;
+        boolean isInt = true;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello! Enter numbers to add to the counter.");
+        System.out.println("Hello! Enter integers to add to the counter. If you want to stop, type 'break'.");
+        while (isInt) {
+            if (sc.hasNextInt()) {
+                counter += sc.nextInt();
+                System.out.println("Counter: " + counter);
+            } else {
+                System.out.println("Oops! That was not an int!\nCounter: " + counter);
+                break;
+            }
+        }
+        sc.close();
     }
 }
