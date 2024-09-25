dbc5c5d71fb8f239693b32e04fe20341e1140196
Catherine <catlih28@gmail.com>
2024-09-25 18:56:55 -0400

    imported the java.util package, created a String 'input' and a Scanner object 'sc'

---
+++ Assignment1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Assignment1 {
+    public static void main(String[] args) {
+        int counter = 0;
+        String input;
+        Scanner sc = new Scanner(System.in);
+    }
+}
