e193c547ad0d0306b892ccefbe8e3c5755d3e08f
Catherine <catlih28@gmail.com>
2024-09-25 19:40:15 -0400

    changed print message when user doesn't enter an int because I didn't like the last one

--- Assignment1.java
+++ Assignment1.java
@@ -2,15 +2,18 @@ import java.util.*;
 public class Assignment1 {
     public static void main(String[] args) {
         int counter = 0;
+        String input;
         boolean isInt = true;
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! Enter integers to add to the counter. If you want to stop, type 'break'.");
+        
         while (isInt) {
             if (sc.hasNextInt()) {
                 counter += sc.nextInt();
                 System.out.println("Counter: " + counter);
-            } else {
-                System.out.println("Oops! That was not an int!\nCounter: " + counter);
+            } 
+            else {
+                System.out.println("Thanks for playing!\nCounter: " + counter);
                 break;
             }
         }
