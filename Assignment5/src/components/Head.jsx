699f8018284a245bb90a1b211f01dada8192224d
Catherine Huang <catherine.huang5@student.tdsb.on.ca>
2024-11-27 11:27:06 -0500

    fixed home screen

--- Assignment5/src/components/Head.jsx
+++ Assignment5/src/components/Head.jsx
@@ -15,10 +15,10 @@ function Header() {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
             <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
             
-            <div class="background">
+            <div class="background"></div>
             <h class="Popflix">Popflix</h>
 
-            <div class="homepage"></div>
+            <div class="homepage">
                 <h1>Endless popcorn binging</h1>
                 <p class="startsatpi-text">Starts at $3.14. Cancel anytime.</p>
 
