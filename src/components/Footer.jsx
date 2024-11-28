e88c362e454070d73ba227b5ace14c218d9b2ff5
Catherine Huang <catherine.huang5@student.tdsb.on.ca>
2024-11-28 11:37:31 -0500

    home view

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div>
+            <div class="footer">
+                <h class="aboutus">About Us</h>
+            </div>
+            <div class="footerlinks">
+                <a href="https://www.netflixinyourneighbourhood.ca/privacy/" class="TC" target="_blank">Terms & Conditions</a>
+                <a href="https://github.com/pepecatt/Assignment-2.git" class="github" target="_blank">GitHub</a>
+                <a href="https://youtu.be/G3e-cpL7ofc?si=x_oIzjVSF9kqM3-R" class="tutorial" target="_blank">HTML + CSS Tutorial</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,18 @@
+import "./HomeView.css";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Feature from "../components/Feature";
+import Footer from "../components/Footer";
+
+function HomeView() {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Feature />
+            <Footer />
+        </>
+    )
+}
+
+export default HomeView;
